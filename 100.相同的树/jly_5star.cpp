362845e23fd2ac0242a0ca72babd46c94a8dce7d
jinluyang <1055057679@qq.com>
2020-03-14 20:37:51 +0800

    简单的递归

---
+++ 100.相同的树/jly_5star.cpp
@@ -0,0 +1,61 @@
+/*
+给定两个二叉树，编写一个函数来检验它们是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+示例 1:
+
+输入:       1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+输出: true
+
+示例 2:
+
+输入:      1          1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+输出: false
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/same-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+执行用时 :
+0 ms
+, 在所有 C++ 提交中击败了
+100.00%
+的用户
+内存消耗 :
+12.2 MB
+, 在所有 C++ 提交中击败了
+5.06%
+的用户
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL)
+            return true;
+        if (p == NULL || q == NULL)
+            return false;
+        if (isSameTree(p->left, q->left) && isSameTree(p->right, q->right) && p->val == q->val)
+            return true;
+        return false;
+
+    }
+};
