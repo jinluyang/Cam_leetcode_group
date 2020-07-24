15411493ec1e2cdabbed40c1f28040db148fdcbf
jinluyang <1055057679@qq.com>
2020-07-24 18:40:52 +0800

    动态规划，约数

---
+++ 1025.除数博弈/jly.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> getFactor(int N) {
+        vector<int> res = {1};
+        for (int i = 2; i <= N/2 + 1; i++) {
+            if (N % i == 0)
+                res.push_back(i);
+        }
+        return res;
+    }
+    bool divisorGame(int N) {
+        vector<bool> result;
+        result.push_back(false);
+        result.push_back(false);//1
+        result.push_back(true);//2
+        for (int i = 3; i <= N; i++) {
+            if (result[i-1] == false)
+                result.push_back(true);
+            else {
+                //cout << "here";
+                vector<int> factors = getFactor(i);
+                bool found = false;
+                for (int factor : factors) {
+                    if (result[i-factor] == false) {
+                        result.push_back(true);
+                        found = true;
+                    }
+                }
+                if (!found)
+                    result.push_back(false);
+
+            }
+
+        }
+        return result[N];
+    }
+};
